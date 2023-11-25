5318e4da29f047d9e94d0d01de30c992eebae38b
leo929 <leo929@naver.com>
2023-11-25 15:01:06 +0900

    map
    map

--- Assets/Scripts/MapCreateScripts/MapCreator.cs
+++ Assets/Scripts/MapCreateScripts/MapCreator.cs
@@ -7,11 +7,11 @@ public class MapCreator : MonoBehaviour
 {
     public static Transform onPlayerMap;//플레이어가 위치한 땅 이 땅이 currentMapObject[1,1]과 다르다면 플레이어가 이동한 것
     public static bool center;
-    Ray checkerRay;
-    RaycastHit hit;    //같은 곳에 맵을 성성하지 않도록 땅이 있는지 확인하는 체커
+    Ray checkerRay;    //같은 곳에 맵을 성성하지 않도록 땅이 있는지 확인하는 체커
     public static bool[,] mapArray = new bool[3, 3];//땅을 만들때 true를 넣어줌
     public static Transform[,] currentMapObject = new Transform[3, 3]; //플레이어가 어디로 이동했는지 알기 위해 각 땅의 정보를 담을 배열
     public Transform map; //맵
+    Transform hitTemp;
 
     // Start is called before the first frame update
     void Start()//플레이어가 오브젝트와 겹치는 문제가 있어 시작할때 플레이어의 자리에 땅을 하나 만들어줌
@@ -21,17 +21,9 @@ public class MapCreator : MonoBehaviour
         onPlayerMap = MapInstance;
         mapArray[1, 1] = true;
         center = true;
-        for (int z = 0; z < 3; z++) //처음엔 모두 false이기 때문에 플레이어 주변 8칸에 맵이 생성됨
-        {
-            for(int x = 0; x < 3; x++)
-            {
-                if (mapArray[z, x] == false)
-                {
-                    MapCreate(z, x);
-                }
-            }
-        }
+        MapCreate();
         currentMapObject[1, 1] = onPlayerMap; // 플레이어의 위치는 오브젝트 배열상에서 언제나 [1,1]에 위치
+        hitTemp = onPlayerMap.transform;
     }
 
     // Update is called once per frame
@@ -39,11 +31,21 @@ public class MapCreator : MonoBehaviour
     {
         //this.gameObject.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);//맵 생성기가 언제나 각 맵의 중앙에 위치하도록 해줌
         //this.gameObject.transform.position = onPlayerMap.transform.position;
+        //PlayerMoved();
+        RaycastHit hit;
+        Debug.DrawRay(transform.position + new Vector3(0.0f, 0.5f, 0.2f), Vector3.down * 1.0f, Color.red);
+        if(Physics.Raycast(transform.position + new Vector3(0.0f, 0.5f, 0.2f), Vector3.down, out hit, 1.0f))
+        {
+            if(hitTemp != hit.transform)
+            {
+                onPlayerMap = hit.transform;
+                MapCheck();
+            }
+        }
+        hitTemp = hit.transform;
     }
-    void PlayerMoved(Collider coll)
+    void PlayerMoved()
     {
-        Debug.Log("moved");
-        onPlayerMap = coll.transform;
         if(onPlayerMap == currentMapObject[1, 1])//플레이어가 중앙에 있는지 확인
             center = true;
         if(onPlayerMap != currentMapObject[1, 1])//중앙이 아니라면 어느곳으로 이동했는지 확인
@@ -83,65 +85,78 @@ public class MapCreator : MonoBehaviour
                 }
             }
         }
-        for(int z = 0; z < 3; z++)
+    }
+    void MapCheck()
+    {
+        RaycastHit hit;
+        for(int x = 0; x < 3; x++)
         {
-            for(int x = 0; x < 3; x++)
+            for(int z = 0; z < 3; z++)
             {
-                if(mapArray[z, x] == false)//플레이어가 움직였고 맵을 만들 곳에 맵이없다면 맵을 생성해줌
-                    MapCreate(z, x);
+                mapArray[x, z] = false;
+                if(Physics.Raycast(onPlayerMap.transform.position + new Vector3(0.3f * x - 0.3f, 1.0f, 0.3f * z - 0.3f), Vector3.down, out hit, 1.0f))
+                {
+                    Debug.DrawRay(onPlayerMap.transform.position + new Vector3(0.3f * x - 0.3f, 1.0f, 0.3f * z - 0.3f), Vector3.down * 1.0f, Color.red);
+                    mapArray[x, z] = true;
+                    for(int a = 0; a < 3; a++)
+                    {
+                        for(int b = 0; b < 3; b++)
+                        {
+                            Debug.Log(mapArray[a, b]);
+                        }
+                    }
+                }
+            }
+        }
+        MapCreate();
+    }
+    void MapCreate()
+    {
+        for(int x = 0; x < 3; x++)
+        {
+            for(int z = 0; z < 3; z++)
+            {
+                if(mapArray[x, z])
+                    continue;
+                else
+                {
+                    Transform MapInstance;
+                    MapInstance = Instantiate(map);
+                    MapInstance.transform.position = onPlayerMap.transform.position;//플레이어가 위치한 땅으로 인스턴스의 위치 초기화
+                    switch (z)  //z의 값에 따라 인스턴스의 위치 조절 
+                    {
+                        case 0:
+                            MapInstance.transform.position += new Vector3(0.0f, 0.0f, -0.3f);
+                            break;
+                        case 1:
+                            MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.0f);
+                            break;
+                        case 2:
+                            MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.3f);
+                            break;
+                    }
+                    switch (x)  //x의 값에 따라 인스턴스의 위치 조절 
+                    {
+                        case 0:
+                            MapInstance.transform.position += new Vector3(-0.3f, 0.0f, 0.0f);
+                            break;
+                        case 1:
+                            MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.0f);
+                            break;
+                        case 2:
+                            MapInstance.transform.position += new Vector3(0.3f, 0.0f, 0.0f);
+                            break;
+                    }
+                    currentMapObject[x, z] = MapInstance;
+                    mapArray[x, z] = true;  //만들었으니 배열에 표시해줌
+                }
             }
         }
     }
-    void MapCreate(int z, int x)
-    {
-        Transform MapInstance;
-        MapInstance = Instantiate(map);
-        MapInstance.transform.position = onPlayerMap.transform.position;//플레이어가 위치한 땅으로 인스턴스의 위치 초기화
-        mapArray[z, x] = false;
-        if(Physics.Raycast(MapInstance.transform.position + new Vector3(0.3f * z - 0.3f, 1.0f, 0.3f * x - 0.3f), Vector3.down, out hit, 1.0f))
-        {
-            mapArray[z, x] = true;
-            // for(int a = 0; a < 3; a++)
-            // {
-            //     for(int b = 0; b < 3; b++)
-            //     {
-            //         Debug.Log(mapArray[a, b]);
-            //     }
-            // }
-        }
-        Debug.DrawRay(MapInstance.transform.position + new Vector3(0.3f * z - 0.3f, 1.0f, 0.3f * x - 0.3f), Vector3.down * 1.0f, Color.red);
-        //int z = index / 3;  //index를 축으로 변환해줌
-        //int x = index % 3;
-        switch (z)  //i(z)의 값에 따라 인스턴스의 위치 조절 
-        {
-            case 0:
-                MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.3f);
-                break;
-            case 1:
-                MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.0f);
-                break;
-            case 2:
-                MapInstance.transform.position += new Vector3(0.0f, 0.0f, -0.3f);
-                break;
-        }
-        switch (x)  //j(x)의 값에 따라 인스턴스의 위치 조절 
-        {
-            case 0:
-                MapInstance.transform.position += new Vector3(-0.3f, 0.0f, 0.0f);
-                break;
-            case 1:
-                MapInstance.transform.position += new Vector3(0.0f, 0.0f, 0.0f);
-                break;
-            case 2:
-                MapInstance.transform.position += new Vector3(0.3f, 0.0f, 0.0f);
-                break;
-        }
-        mapArray[z, x] = true;  //만들었으니 배열에 표시해줌
-    }
 
-    void OncollisionEnter(Collider other) 
-    {
-        Debug.Log("asdf");
-        PlayerMoved(other);
-    }
+    // void OnCollisionEnter(Collision other) 
+    // {
+    //     onPlayerMap = other.transform;
+    //     MapCheck();
+    // }
 }
